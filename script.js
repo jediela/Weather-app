a02009834f0a7433d1ef8833597afdc65531c1b9
Jed <jediel2424@gmail,com>
2023-07-28 21:56:57 -0400

    Did styling and started on the javascript, need to do the display weather stuff

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 let weather = {
-    "apiKey": "42449f76517895b6ab05cd84ab15842c"
+    apiKey: "42449f76517895b6ab05cd84ab15842c",
 
+    // Fetch weather function
+    fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    },
 
-    // Function that gets information on given city
-    
-
-    // Fuction that displays the weather
-
-}
+    // Display weather function 
+    displayWeather: function(data){
+        const {name} = data;
+        const {icon, descrption} = data.weather;
+        const {temp, humidity} = data.main;
+        
+    }
+};
