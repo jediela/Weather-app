c62afb546b13923af3e9f614f0b36d206eb17e96
Jediel Antalan <jediel2424@gmail.com>
2023-08-03 20:19:57 -0400

    Finished up!

--- script.js
+++ script.js
@@ -7,14 +7,51 @@ let weather = {
             "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey
         )
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => this.displayWeather(data));
     },
 
     // Display weather function 
     displayWeather: function(data){
         const {name} = data;
-        const {icon, descrption} = data.weather;
+        const {icon, description} = data.weather[0];
         const {temp, humidity} = data.main;
+        const{speed} = data.wind;
         
-    }
+        // Round the temperature
+        const roundedTemp = Math.round(temp);
+
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = roundedTemp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/h";
+        
+       document.querySelector(".weather").classList.remove("loading");
+       document.body.style.backgroundImage = "url('https://source.unsplash.com/2000x1000/?" + name + "')"
+    },
+
+    // Search bar
+    search: function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
 };
+
+    // Button
+    document.querySelector(".search button").addEventListener("click", function() {
+        weather.search();
+    });
+
+    // Press enter
+    document.querySelector(".search-bar").addEventListener("keyup", function() {
+        if(event.key == "Enter"){
+            weather.search();
+        }
+    });
+
+    // Load
+    weather.fetchWeather("Toronto");
+    
+
+
+ 
